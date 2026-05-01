6d852654f49f7d927401c68fbd7a254e663ca009
Reymond Godoy <emonawong22@gmail.com>
2026-05-01 12:39:19 +0800

    added limit to 5mb

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
+import { json, urlencoded } from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,8 @@ async function bootstrap() {
     origin: frontendOrigin,
   });
 
+  app.use(json({ limit: "5mb" }));
+  app.use(urlencoded({ extended: true, limit: "5mb" }));
   await app.listen(port);
   console.log(`Stria API running on http://localhost:${port}/api`);
 }
